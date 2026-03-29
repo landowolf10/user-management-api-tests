f9c023ef603498103a8fffdac41f47f62a091e69
Orlando Avila <orlando.avilag@hotmail.com>
2026-03-28 22:56:48 -0600

    API testing project with Playwright

--- src/fixtures/user.fixture.ts
+++ src/fixtures/user.fixture.ts
@@ -8,16 +8,10 @@ type Env = 'dev' | 'prod';
 
 export const test = base.extend<{
   client: UserClient;
-  env: Env;
 }>({
   client: async ({ request }, use) => {
     const env = (process.env.ENV || 'dev') as Env;
     const client = new UserClient(request, env);
     await use(client);
-  },
-
-  env: async ({ }, use) => {
-    const env = (process.env.ENV || 'dev') as Env;
-    await use(env);
-  },
+  }
 });
